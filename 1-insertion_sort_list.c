8254f68a2f51e8d58de0a3994a8b8b4edc0076d4
campopinillos <1207@holbertonschool.com>
2020-02-03 15:19:57 -0500

    [Fix] Conditional

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *tmp = *list;
 	int temp;
 
-	if (!*list || !list)
+	if (!*list || !list || !(*list)->next->next)
 		return;
 	while (unsorted_list(list))
 	{
