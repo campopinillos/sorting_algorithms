09581c85a2234102192fed4964678c1b0cc2835f
campopinillos <1207@holbertonschool.com>
2020-02-03 17:20:35 -0500

    [Feature] Advanced task

---
+++ 100-shell_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * shell_sort - Sorts an array in ascending order
+ * @array: Array
+ * @size: Pointer to the previous element of the list
+ *
+ * Description: Sorts an array of integers in ascending
+ * order using the Shell sort algorithm
+ * Return: Void
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, i, j;
+	int tmp;
+
+	while (gap <= size)
+		gap = (gap * 3) + 1;
+	gap = (gap - 1) / 3;
+	for ( ; gap > 0; gap = (gap - 1) / 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			tmp = array[i];
+			for (j = i; array[j - gap] > tmp; j -= gap)
+				array[j] = array[j - gap];
+			array[j] = tmp;
+		}
+		print_array(array, size);
+	}
+}
