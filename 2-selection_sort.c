5c345334605999158a0a270d4db5e542bb976413
campopinillos <1207@holbertonschool.com>
2020-02-02 19:30:29 -0500

    [Feature] Project Files

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+int unsorted(int *array);
+/**
+ * selection_sort - Sorts an array in ascending order
+ * @array: Array
+ * @size: Pointer to the previous element of the list
+ *
+ * Description: Sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ * Return: Void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0;
+	int tmp, index;
+
+	for (j = 0; j < size - 2 && array[j + 1]; j++)
+	{
+		tmp = array[j];
+		for (i = j + 1; i < size && array[i]; i++)
+		{
+			if (array[i] < array[j] && array[i] < tmp)
+			{
+				tmp = array[i];
+				index = i;
+			}
+		}
+		if (array[j] > tmp)
+		{
+			array[index] = array[j];
+			array[j] = tmp;
+			print_array(array, size);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -13,9 +13,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* Prototypes */
@@ -25,5 +25,7 @@ void print_list(const listint_t *list);
 /* Prototypes Functions */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
