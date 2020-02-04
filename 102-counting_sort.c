6f52a3c4ce228d7a64e7ec39026c6972f5f4e0f1
campopinillos <1207@holbertonschool.com>
2020-02-04 13:14:16 -0500

    [Fix] Case 2

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,7 +10,7 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int *count, *output = NULL, index;
+	int *count, *output, index;
 	size_t max = array[0], i = 0;
 
 	if (!array || !size || size < 2)
@@ -31,7 +31,7 @@ void counting_sort(int *array, size_t size)
 		count[i] = 0;
 	for (i = 0; i < size; i++)
 		count[array[i]] += 1;
-	for (i = 0; i < max + 1; i++)
+	for (i = 1; i < max + 1; i++)
 		count[i] += count[i - 1];
 	print_array(count, max + 1);
 	for (i = 0; i < size; i++)
@@ -40,7 +40,7 @@ void counting_sort(int *array, size_t size)
 		output[index] = array[i];
 		count[array[i]] -= 1;
 	}
-	for (i = 0; i < size; i++)
+	for (i = size - 1; (int) i >= 0; i--)
 		array[i] = output[i];
 	free(output);
 	free(count);
