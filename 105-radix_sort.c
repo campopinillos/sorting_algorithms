e8c9fef1894f2d01383e718ba879eead58fc67e7
campopinillos <1207@holbertonschool.com>
2020-02-05 15:20:45 -0500

    [Fix] Frees and size

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,5 +1,6 @@
 #include "sort.h"
-int getmax(int *array, size_t size);
+int get_max(int *array, size_t size);
+int get_max_count(int *array, size_t size);
 /**
  * radix_sort - Sorts an array in ascending order
  * @array: Array
@@ -12,19 +13,18 @@ int getmax(int *array, size_t size);
 void radix_sort(int *array, size_t size)
 {
 	int *new_array, *count, *output;
-	int max, i, j, digits = 10, itter = 1, divisor = 1, try;
+	int max, max2, i, j, digits = 10, itter = 1, divisor = 1, try;
 
 	if (!array || size < 2)
 		return;
-	max = getmax(array, size);
+	max = get_max(array, size);
 	while (max / digits > 0)
 	{	itter++;
 		digits *= 10; }
-	count = malloc(sizeof(int) * 10);
+	max2 = get_max_count(array, size);
+	count = malloc(sizeof(int) * (max2 + 1));
 	if (!count)
 		return;
-	for (i = 0; i < 10; i++)
-		count[i] = 0;
 	new_array = malloc(sizeof(int) * (size));
 	if (!new_array)
 		return;
@@ -33,6 +33,8 @@ void radix_sort(int *array, size_t size)
 		return;
 	for (try = 0; try < itter; try++, divisor *= 10)
 	{
+		for (i = 0; i < max2; i++)
+			count[i] = 0;
 		for (j = 0; j < (int) size; j++)
 		{	new_array[j] = ((array[j]) / divisor) % 10;
 			count[new_array[j]] += 1; }
@@ -43,27 +45,46 @@ void radix_sort(int *array, size_t size)
 			count[new_array[i]] -= 1; }
 		for (i = 0; i < (int) size; i++)
 			array[i] = output[i];
-		for (i = 0; i < 10; i++)
-			count[i] = 0;
 		print_array(array, size); }
-}
+	free(count);
+	free(new_array);
+	free(output); }
 
 /**
- * getmax - Maxim number in array
+ * get_max - Maxim number in array
  * @array: Array
  * @size: Size of the array
  *
- * Description: Sorts an array of integers in ascending
- * order using the Radix sort algorithm
  * Return: Void
  */
-int getmax(int *array, size_t size)
+int get_max(int *array, size_t size)
 {
-	int max = array[0], i = 0;
+	int max = array[0], i = 1;
 
 	while (i < (int) size)
 	{
 		if (array[i] > (int) max)
+			max = array[i];
+		i++;
+	}
+	return (max);
+}
+
+/**
+ * get_max_count - Maxim module number in array
+ * @array: Array
+ * @size: Size of the array
+ *
+ * Return: Void
+ */
+int get_max_count(int *array, size_t size)
+{
+	int max, digits = 1, i = 1;
+
+	max = (array[0] / digits) % 10;
+	while (i < (int) size)
+	{
+		if (((array[i] / digits) % 10) > max)
 			max = array[i];
 		i++;
 	}
